2be5192a7ee8b582eedc3050b8a238fc641cfb74
aspzs <73506672+aspzs@users.noreply.github.com>
2022-11-04 20:51:05 -0500

    Update intern.test.js
    Adding the Id that I forgot to uploaded

--- test/intern.test.js
+++ test/intern.test.js
@@ -2,18 +2,18 @@ const Intern = require ("../src/src/lib/Intern");
 
 test("Should set School name via constructor", () => {
     const testSchool = "test";
-    const intern = new Intern("Arnol", "25sparnol@gmail.com", testSchool);
+    const intern = new Intern("Arnol", 123, "25sparnol@gmail.com", testSchool);
     expect(intern.school).toBe(testSchool);
   });
   
   test("Should return the Intern role with the getRole method", () => {
     const testRole = "Intern";
-    const intern = new Intern("Arnol", "25sparnol@gmail.com", "test");
+    const intern = new Intern("Arnol", 123, "25sparnol@gmail.com", "test");
     expect(intern.getRole()).toBe(testRole);
   });
   
   test("Should return the School name with the getSchool method", () => {
     const testSchool = "test";
-    const intern = new Intern("Arnol", "25sparnol@gmail.com", testSchool);
+    const intern = new Intern("Arnol", 123, "25sparnol@gmail.com", testSchool);
     expect(intern.getSchool()).toBe(testSchool);
   });
